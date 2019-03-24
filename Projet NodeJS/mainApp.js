5a53f69aca643a3e499ba1320dffb1f6e45a165a
Rémy Cordeau-Mirani <rcordeau@enssat.fr>
2019-03-24 10:58:02 +0100

    session verifier

--- Projet NodeJS/mainApp.js
+++ Projet NodeJS/mainApp.js
@@ -13,12 +13,15 @@ mongoose.connection
         console.log(`Connection error: ${err.message}`);
     });
 
-
+/**
+imports and global definitions
+*/
 
 const express = require('express')
 const app = express();
 const port = 3000;
 var bodyParser = require("body-parser");
+var session = require('express-session');
 
 
 const server = app.listen(3000, () => {
@@ -26,28 +29,90 @@ const server = app.listen(3000, () => {
 });
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.use(session({  //to use cookies
+  secret: 'secret text',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+app.use(bodyParser.urlencoded({ extended: true })); // to use bodyParser
+app.set('view engine', 'ejs'); //to use ejs
+
+/**
+  function that checks whether a user is associated with the current session through the username field
+*/
+function checkAuth(req, res) {
+  if (req.session.username == null) {
+    console.log("User not logged in");
+    return false;
+  }
+  return true;
+}
+
 
 /**
 Routes
 */
 
-app.get('/', (req, res) => res.render('login',{}))
+//login page
+
+app.get('/', function(req, res){
+  res.render("login");
+});
+
+//chooseGame page
+
+app.get('/chooseGame', function(req,res){ // if the user is not logged in, he can't directly access the chooseGame page, he will be redirected to the login page
+    if(!checkAuth(req, res)){
+      res.render("login",{});
+    }
+    else{
+    res.render("chooseGame",{});
+  }
+  });
 
 app.post('/chooseGame', function(req, res) {
+  req.session.username = req.body.name;
+  //console.log(req.session.username);
   res.render('chooseGame',{username: req.body.name})
 });
 
+app.get( "/disconnect", (req,res) =>{ //disconnect button, the req.session.username field is deleted
+  req.session = null; //destroy session
+  res.render("login",{});
+});
+
+// game1 page
+
 app.get('/game1', function(req, res) {
-  res.render('game1',{})
+  console.log(checkAuth(req,res));
+  if(!checkAuth(req, res)){
+    res.render("login",{});
+    }
+    else{
+      res.render('game1',{});
+    }
 });
 
 app.post('/game1', function(req, res) {
+  req.session.username = req.body.name;
   console.log("username :"+req.body.name+" userscore : "+req.body.score);
-  res.render('game1',{username: req.body.name},{userscore: req.body.score})
+  res.render('game1',{username: req.body.name,userscore: req.body.score})
 });
 
+//gameNotAvailable page
+
+app.get("/gameNotAvailable", function (req,res) {
+  if(!checkAuth(req, res)){
+    res.render("login",{});
+    }
+    else{
+        res.render("gameNotAvailable",{});
+    }
+})
+
 app.post("/gameNotAvailable", function(req,res){
+  req.session.username = req.body.name;
+  //console.log(req.session.username);
   res.render('gameNotAvailable', {username: req.body.name})
 });
